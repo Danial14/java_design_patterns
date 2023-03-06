92fba4f770f16610dd11564fe951f667957b5512
softchanpk <sc.indus.erp@gmail.com>
2023-03-06 18:22:21 +0500

    updated

---
+++ prototype/Prototype_demo.java
@@ -0,0 +1,8 @@
+package prototype;
+
+public class Prototype_demo {
+    public static void main(String[] args) {
+        
+    }
+    
+}
---
+++ prototype/Shape.java
@@ -0,0 +1,27 @@
+package prototype;
+
+public abstract class Shape implements Cloneable {
+    private String id;
+    protected String type;
+    public abstract void draw();
+    public Object clone(){
+        // TODO Auto-generated method stub
+        Object clone = null;
+        try{
+            clone = super.clone();
+        }
+        catch(CloneNotSupportedException e){
+            System.out.println(e.getMessage());
+        }
+        return clone;
+    }
+    public String getType(){
+        return type;
+    }
+    public String getId(){
+        return id;
+    }
+    public void setId(String id){
+        this.id = id;
+    }
+}
